6707e7572486b3a73f567b40b6ffbf9bd0c3a536
JESSIE POMAA AMANKWAH <140706046+ExpertGrim@users.noreply.github.com>
2025-08-03 00:12:53 +0200

    Add files via upload

---
+++ DB.py
@@ -0,0 +1,15 @@
+file2 = "File2.txt"
+file = []
+
+try:
+    with open('file.txt') as file:
+        
+        
+        
+    with open (file2,'w') as file:
+        
+            
+        
+except FileNotFoundError:
+    print("Thats not a file")
+    
