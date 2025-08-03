7308a5cdf9d9a55fbb9314038d75cdda02d7550f
JESSIE POMAA AMANKWAH <140706046+ExpertGrim@users.noreply.github.com>
2025-08-03 15:57:54 +0200

    updates

--- DB.py
+++ DB.py
@@ -1,17 +1,32 @@
 
 
 file2 = "File2.txt" #creating a variable for the output file
-file = []
+data = []
 
 try:
     with open('file.txt','r',encoding='utf-8') as file:
-        for line in file:
-          base = file.readlines()
+        header = file.readline().strip().split(",") #to take the header from the top of the file
+        rows = [dict(zip(header, line.strip().split(","))) for line in file]# this is to take the header and than make rows for each data to make it easily accesbile in a dictionary set
+    #print(rows)
         
-        
-        
-    with open (file2,'w') as file:
-        pass
+    with open (file2,'w') as file: #to create the file where the answers are 
+        file.write("Question a: \n ")
+        for row in rows:
+            country = row['CountryName']
+            if country.endswith("a"):
+                file.write(f"{country},")
+        citypop = [] 
+        for row in rows:
+            if 'CityPopulation' in row:
+                pop = int(row['CityPopulation'])
+                citypop.append(pop)
+        print(citypop)
+        city5 = sorted(citypop, reverse=True)[:5]
+        file.write("\n\nQuestion b: \n " + str(city5))
+
+                  
+                
+         
             
         
 except FileNotFoundError:
